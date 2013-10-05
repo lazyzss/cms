c886a7f5f16496a9ac4339b6db04fa2a118c500c
daiyu <61948037@qq.com>
2013-10-05 17:57:44 +0800

    添加了Post文章实例类的扩展类Postmeta的临时版本

--- src/main/java/cn/daryu/cms/repository/UserDao.java
+++ src/main/java/cn/daryu/cms/repository/UserDao.java
@@ -1,6 +1,6 @@
 package cn.daryu.cms.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import cn.daryu.cms.entity.User;
 
@@ -10,7 +10,7 @@ import cn.daryu.cms.entity.User;
  * @author Day
  *
  */
-public interface UserDao extends CrudRepository<User, Long>{
+public interface UserDao extends PagingAndSortingRepository<User, Long>{
 
 
 }
